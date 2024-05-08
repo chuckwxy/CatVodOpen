9595eb2e67679cf82d3a8cf9c9ec91f1c06ad80c
chuckwxy <144194226+chuckwxy@users.noreply.github.com>
2024-05-08 13:31:04 +0800

    Update kunyu77.js

--- nodejs/src/spider/video/kunyu77.js
+++ nodejs/src/spider/video/kunyu77.js
@@ -18,7 +18,7 @@ WA45MmpCSSgjVvX4bbV43IjdjSZNm8s5efdlLl1Z+7uJTR024xizhK5NH0/uPmR4
 O8QEtxO9ha3LMmTYTfERzfNmpfDVdV3Rok4eoTzhHmxgqQ0/S0S+FgjHiwrCTFlv
 NCiDhSemnJT+NIzAnMQX4acL5AYNb5PiDD06ZMrtklTua+USY0gSIrG9LctaYvHR
 swIDAQAB
------END PUBLIC KEY-----`，
+-----END PUBLIC KEY-----`,
     'pkcs8-public-pem',
     {
         encryptionScheme: 'pkcs1',
